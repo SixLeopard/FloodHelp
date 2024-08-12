4316f995e336d474a5ce7ecc712cd31c58591121
randy622 <126127257+randy622@users.noreply.github.com>
2024-08-12 20:11:03 +1000

    Update ExtApis.py
    Now outputs data in the form of a pandas data base (only first 20 rows). Used the "Flood — Awareness — Flood Risk Overall" dataset from 2013.

--- BackEnd/ExtApis.py
+++ BackEnd/ExtApis.py
@@ -1,10 +1,48 @@
 import requests
+from geopy.geocoders import Nominatim
+import pandas as pd
 
+
+#function that takes lon and lat, gives back suburb and street
+def get_location_name(latitude, longitude):
+    geolocator = Nominatim(user_agent="FloodApplicationUQ")
+    location = geolocator.reverse((latitude, longitude), exactly_one=True)
+    
+    if location:
+        
+        address = location.raw['address']
+        road = address.get('road', '')      
+        suburb = address.get('suburb', '')  
+        city = address.get('city', '')      
+        return f"{road}, {suburb or city}"
+    else:
+        return "Location not found"
+    
+
+
+#Getting data
 response = requests.get('https://data.brisbane.qld.gov.au/api/explore/v2.1/catalog/datasets/flood-awareness-flood-risk-overall/records?limit=20')
 data = response.json()
+data_list = []
 
+#Extracting from the data useful information (lon, lat, flood type, flood risk)
 for result in data['results']:
-    for key in result.keys():
-        print(key, " = ", result[key])
-        print()
-        print()
+    keys = list(result.keys())  
+    risk = result[keys[1]]
+    type = result[keys[2]]
+    if (result[keys[-1]]) is not None:
+        new_keys = list((result[keys[-1]]).keys())
+        lon = (result[keys[-1]])[new_keys[0]]
+        lat = (result[keys[-1]])[new_keys[1]]
+        location_name = get_location_name(lat, lon)
+
+        data_list.append({
+            'location_name': location_name,
+            'type': type,
+            'risk': risk
+        })
+
+
+
+df = pd.DataFrame(data_list)
+print(df)
