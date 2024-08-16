d162de864d47b657b8a3cd12396af8053fb513c4
Jamie <SixLeopard75798@gmail.com>
2024-08-16 22:31:15 +1000

    updated login system (works the same but just cleaner now) add basic user reporting functionality and start on notification system

--- BackEnd/API/Login.py
+++ BackEnd/API/Login.py
@@ -3,6 +3,7 @@ from flask import Flask, session, make_response,request, Blueprint
 from flask_session import Session
 import os
 import base64
+import uuid
 
 #for password encryption
 from cryptography.fernet import Fernet
@@ -14,52 +15,75 @@ login_routes = Blueprint("login_routes", __name__)
 #switch this to actually user database
 accounts =	{}
 
+session_username_mappings = {}
+
+def verify_user_account(username, session):
+    try:
+        if session_username_mappings[username].decode() == session.decode():
+            return True
+        else:
+            return False
+    except:
+        return False
 
 @login_routes.route("/account/login", methods = ['POST'])
 def login():
     if request.method == 'POST':
+        #get uername and password from api call
         username = request.form.get('username')
         password = request.form.get('password')
+
+        #if valid username
         if username in accounts:
+            #set up encryption allocation and get saved salt for user
             salt = accounts[username][1]
-            kdf = PBKDF2HMAC(
-                algorithm=hashes.SHA256(),
-                length=32,
-                salt=salt,
-                iterations=480000,
-            )
-            key = base64.urlsafe_b64encode(kdf.derive(password.encode()))
-            if key == accounts[username][2]:
-                session['id'] = accounts[username][0]
-                return make_response({"Login":"True",
-                                    "sessionid":f"{session['id']}"
-                                    })
-        return make_response({"Login":"False"
-                                })
+            kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),length=32,salt=salt,iterations=480000)
+
+            #generate the passkey by encoding the password
+            passkey = base64.urlsafe_b64encode(kdf.derive(password.encode()))
+
+            #check to see if username and password match
+            if passkey == accounts[username][0]: #**** change this to check database instead ****#
+                #generate session key
+                sessionkey = Fernet(passkey).encrypt(uuid.uuid4().bytes)
+
+                #assign session id and username
+                session['id'] = sessionkey
+                session['username'] = username
+
+                #setup active username to session key mapping
+                session_username_mappings[session['username']] = session['id']
+
+                #return the session id on succesful login
+                return make_response({"Login":"True", "sessionid":f"{session['id']}"})
+            
+        #if login fails
+        return make_response({"Login":"False"})
 
 @login_routes.route("/account/create", methods = ['POST'])
 def create():
     if request.method == 'POST':
+        #get uername and password from api call
         username = request.form.get('username')
         password = request.form.get('password')
+
+        #set up encryption allocation and generate salt for user
         salt = os.urandom(16)
-        kdf = PBKDF2HMAC(
-            algorithm=hashes.SHA256(),
-            length=32,
-            salt=salt,
-            iterations=480000,
-        )
-        key = base64.urlsafe_b64encode(kdf.derive(password.encode()))
-        sessionkey = Fernet(key).encrypt(username.encode())
+        kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),length=32,salt=salt,iterations=480000)
+
+        #generate the passkey by encoding the password
+        passkey = base64.urlsafe_b64encode(kdf.derive(password.encode()))
+
+        #make sure user dosent already exist
         if username not in accounts:
-            session['id'] = key
-            accounts[username] = [sessionkey,salt,key]
-            return make_response({"created":"True",
-                                "username":f"{username}",
-                                "passkey":f"{sessionkey}"
-                                })
-        return make_response({"created":"False"
-                                })
+            #**** change to save to database ****#
+            accounts[username] = [passkey,salt]
+
+            #reutrn infomration
+            return make_response({"created":"True","username":f"{username}","passkey":f"{passkey}"})
+        
+        #if create failed
+        return make_response({"created":"False"})
     
 @login_routes.route('/login/test')
 def get_data():
--- BackEnd/server.py
+++ BackEnd/server.py
@@ -16,6 +16,7 @@ import os
 import API.Login as APIlogin
 import API.Session as APIsession
 import API.Notifications as APINotifications
+import API.UserReport as UserReport
 
 # Initializing flask app
 app = Flask(__name__)
@@ -23,7 +24,7 @@ app = Flask(__name__)
 #addin externals
 app.register_blueprint(APIlogin.login_routes)
 app.register_blueprint(APIsession.session_routes)
-app.register_blueprint(APINotifications.notifications_routes)
+app.register_blueprint(UserReport.userreport_routes)
 
 #Session
 SESSION_TYPE = 'cachelib'
