37821e19a8497240bf1675ef7e495ba39f2617ac
Jamie <SixLeopard75798@gmail.com>
2024-08-22 23:53:35 +1000

    finished of notifications + general cleanup + added setup to backend readme

--- BackEnd/API/Notifications.py
+++ BackEnd/API/Notifications.py
@@ -1,31 +1,58 @@
 #flask
 from flask import Flask, session, make_response,request, Blueprint
-import API.Login as Login
+import API.Accounts as Accounts
 
 notifications_routes = Blueprint("notifications_routes", __name__)
 
 #switch to using database
 pending_notifications = {}
 
-@notifications_routes.route("/add_notification")
-def add_notification_external():
+
+def add_notification(receiver : str, notification : str) -> None:
+    if receiver in pending_notifications:
+        pending_notifications[receiver].append(notification)
+    else:
+        pending_notifications[receiver] = [notification]
+
+def get_notification(receiver : str) -> list[str]:
+    if receiver in pending_notifications:
+        return pending_notifications[receiver]
+    else:
+        return []
+
+def clear_notification(receiver : str) -> None:
+    if receiver in pending_notifications:
+        pending_notifications[receiver] = []
+
+@notifications_routes.route("/notifications/add", methods = ['POST'])
+def add_notification_route():
     '''
-        submit new user report
+        submit new pending notification
     '''
     if request.method == 'POST':
         notification = request.form.get('notification')
         receiver = request.form.get('receiver')
-        if Login.verify_user_account(session["username"], session["id"]):
-            if receiver in pending_notifications:
-                pending_notifications[receiver].append(notification)
-            else:
-                pending_notifications[receiver] = [notification]
-            
-            return make_response("{notifcation added: " + notification + "}")
+        if Accounts.verify_user_account(session["username"], session["id"]):
+            add_notification(receiver,notification)
+
+            return make_response("{notifcation added: " + str(notification) + "}")
         
         return make_response({"invalid_account":1})
     
     return make_response({"invalid_request":1})
 
-def add_notification_local(session_id : int, nofication : str):
-    return None
+
+@notifications_routes.route("/notifications/get", methods = ['GET'])
+def get_notification_route():
+    '''
+        get all pending notification
+    '''
+    if request.method == 'GET':
+        if Accounts.verify_user_account(session["username"], session["id"]):
+            users_pending_notifications = get_notification(session["username"])
+            clear_notification(session["username"])
+            return make_response("{current pending notifications: " + str(users_pending_notifications) + "}")
+        
+        return make_response({"invalid_account":1})
+    
+    return make_response({"invalid_request":1})
