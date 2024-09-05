91387b66690d5a03ad284b85a8c6cc754bffd658
Jamie Westerhout <SixLeopard75798@gmail.com>
2024-09-06 09:10:13 +1000

    add proximity comments

--- BackEnd/Tools/Proximity.py
+++ BackEnd/Tools/Proximity.py
@@ -1,9 +1,22 @@
 import numpy as np
 
-LAT = 0
-LONG = 1
+LAT = 0 #index of latitude
+LONG = 1 # index of longatude
 
 def is_close(base_location : tuple[int,int], test_location : tuple[int,int], bound : int):
+    '''
+    return wether the distance between base location adn test location is less than the
+    bound distance
+
+    paramters:
+        test_location:
+            tuple of the form (lat,long)
+            the loction the test the distance from the base loation
+        bas_location:
+            tuple of the form (lat,long)
+            the location to tset how far the test distance is fromm
+    '''
+    # sqrt((Alat-Blat)^2+(Along-Blong)^2)
     if \
     np.sqrt(
         np.square(
@@ -14,7 +27,7 @@ def is_close(base_location : tuple[int,int], test_location : tuple[int,int], bou
                 base_location[LONG]-test_location[LONG]
                 )
         ) \
-    <= bound:
+    <= bound: #when in bound true else false
         return True
     else:
         return False
