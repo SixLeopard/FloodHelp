cacf9ba7c9ec513727c89327751ba43ab40e3970
justgenetic <47243984+justgenetic@users.noreply.github.com>
2024-09-06 08:29:32 +1000

    setup of API functionality on front end

--- FrontEnd/flood-help/app/(tabs)/apiTesting.js
+++ FrontEnd/flood-help/app/(tabs)/apiTesting.js
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 import useStyles from "@/constants/style";
 import getAPI from "../GetAPI";
@@ -6,8 +5,17 @@ import getAPI from "../GetAPI";
 export default function ApiTesting() {
     const styles = useStyles();
 
-    const URL = 'https://aurumgrallarius.com/mytransport/api/findstops';
-    const body = {"lat": -27.4962819, "lon": 153.0139074, "limit": 20}
+    // Change these constants
+
+    // const URL = 'https://aurumgrallarius.com/mytransport/api/findstops';
+    // const body = {"lat": -27.4962819, "lon": 153.0139074, "limit": 20}
+    // const method = 'POST'
+
+    const URL = 'http://54.206.190.121:5000/reporting/user/get_all_report';
+    const body ={
+        "Login": "True",
+        "sessionid": "b'gAAAAABm1Dler3oe5IFx7NB_fFQPcznmmVTaoO6E6h0766nN9TS7MSfNRSRkYTb3PbNgPggiY1rk3Mx26ch3B1fzGmevG3xr6oSv9oTNo408gJPRoBKZISg='"
+    }
     const method = 'POST'
 
    const result = getAPI(URL, method, body);
