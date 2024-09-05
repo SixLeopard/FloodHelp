023d3b0b5046cd094aeaeeb606013ecd8d03da5b
justgenetic <47243984+justgenetic@users.noreply.github.com>
2024-09-06 09:01:05 +1000

    minor fixes

--- FrontEnd/flood-help/app/(tabs)/apiTesting.js
+++ FrontEnd/flood-help/app/(tabs)/apiTesting.js
@@ -5,18 +5,12 @@ import getAPI from "../GetAPI";
 export default function ApiTesting() {
     const styles = useStyles();
 
-    // Change these constants
+    // Change these constants to our project variables
 
-    // const URL = 'https://aurumgrallarius.com/mytransport/api/findstops';
-    // const body = {"lat": -27.4962819, "lon": 153.0139074, "limit": 20}
-    // const method = 'POST'
-
-    const URL = 'http://54.206.190.121:5000/reporting/user/get_all_report';
-    const body ={
-        "Login": "True",
-        "sessionid": "b'gAAAAABm1Dler3oe5IFx7NB_fFQPcznmmVTaoO6E6h0766nN9TS7MSfNRSRkYTb3PbNgPggiY1rk3Mx26ch3B1fzGmevG3xr6oSv9oTNo408gJPRoBKZISg='"
-    }
+    const URL = 'https://aurumgrallarius.com/mytransport/api/findstops';
+    const body = {"lat": -27.4962819, "lon": 153.0139074, "limit": 20}
     const method = 'POST'
+
 
    const result = getAPI(URL, method, body);
     return (
