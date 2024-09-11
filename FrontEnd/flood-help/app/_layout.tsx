9d497ae1d249df296eafda1cf2d479c3342a21fa
justgenetic <47243984+justgenetic@users.noreply.github.com>
2024-09-11 20:32:35 +1000

    not working yet

--- FrontEnd/flood-help/app/_layout.tsx
+++ FrontEnd/flood-help/app/_layout.tsx
@@ -1,37 +1,6 @@
-import { Stack} from 'expo-router/stack';
-import { ThemeProvider } from '@/constants/ThemeProvider';
-import FontContext from '@/contexts/FontContext';
-import { AuthProvider, useAuth } from '@/contexts/AuthContext';
-import LoadingScreen from "@/app/(auth)/LoadingScreen";
-import LoginScreen from "@/app/(auth)/LoginScreen";
+import {Navigator} from "expo-router";
+import Slot = Navigator.Slot;
 
 export default function RootLayout() {
-    return (
-        <AuthProvider>
-            <AuthenticatedLayout />
-        </AuthProvider>
-    );
-}
-
-// AuthenticatedLayout component checks if the user is logged in
-function AuthenticatedLayout() {
-    const { user, loading } = useAuth(); // Get auth state from context
-
-    if (loading) {
-        return <LoadingScreen />; // Show a loading screen while checking auth status
-    }
-
-    if (!user) {
-        return <LoginScreen/> // Redirect to login if user is not authenticated
-    }
-
-    return (
-        <FontContext>
-            <ThemeProvider>
-                <Stack>
-                    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-                </Stack>
-            </ThemeProvider>
-        </FontContext>
-    );
+    return <Slot />;
 }
