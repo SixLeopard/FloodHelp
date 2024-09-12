0ec0ef9e1d4fba8919da188b0d2c45ab52030526
justgenetic <47243984+justgenetic@users.noreply.github.com>
2024-09-13 08:43:08 +1000

    refactored folder structure and cleaned up a few files.

--- FrontEnd/flood-help/app/(app)/(tabs)/profile.tsx
+++ FrontEnd/flood-help/app/(app)/(tabs)/profile.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import {View, Text, Button, Animated} from "react-native";
-import {useTheme} from "@/constants/ThemeProvider";
+import {View, Text, Animated} from "react-native";
 import useStyles from "@/constants/style";
-import UserCard from "@/components/UserCard";
 import UserAvatar from "@/components/UserAvatar";
 import ReportCard from "@/components/ReportCard";
-import {router} from "expo-router";
-import {Pressable} from "react-native";
 import FH_Button from "@/components/navigation/FH_Button";
 import ScrollView = Animated.ScrollView;
 
 
 const Profile = () => {
     const styles = useStyles();
-    const theme = useTheme();
 
     return (
         <View style={styles.page}>
             <Text style={styles.headerText}>Profile Page</Text>
             <UserAvatar size={100} imageLink={""} />
             <Text style={styles.nameText}>User Name</Text>
-
             <Text style={styles.bodyTextBold }>Reporting History</Text>
             <ScrollView style={{height: "50%", width: "100%"}}>
 <View style={styles.reportCardsContainer}>
