4f724b3e80b1f7762228959fe15e005397776424
Jamie <SixLeopard75798@gmail.com>
2024-09-15 01:34:50 +1000

    user reports mostly fixed but missing user

--- BackEnd/Tools/UserReportVerfication.py
+++ BackEnd/Tools/UserReportVerfication.py
@@ -1,4 +1,5 @@
 import Tools.Proximity as proximity
+import Tools.CoordString_to_Tuple as cordconv
 
 acceptable_proximity = 0.02
 
@@ -18,7 +19,7 @@ def validate_user_reports(sample_reports : dict, base_report : dict) -> tuple[in
     similarity_score = 0
     similar_reports = []
     for i in sample_reports:
-        if proximity.is_close(base_report["location"], sample_reports[i]["location"], acceptable_proximity) and \
+        if proximity.is_close(cordconv.convert(base_report["coordinates"]), cordconv.convert(sample_reports[i]["coordinates"]), acceptable_proximity) and \
             base_report["type"] == sample_reports[i]["type"] and base_report["User"] != sample_reports[i]["User"]:
             similarity_score += 1
             similar_reports.append(i)
