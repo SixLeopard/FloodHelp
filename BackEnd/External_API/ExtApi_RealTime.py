212e3c3bd5e46a773923b0514f16fc37899c2b21
randy622 <randy.chakra@gmail.com>
2024-09-22 17:10:14 +1000

    Added custom fake alert method

--- BackEnd/External_API/ExtApi_RealTime.py
+++ BackEnd/External_API/ExtApi_RealTime.py
@@ -215,7 +215,7 @@ def get_coordinates(location: str) -> tuple:
     else:
         return (None, None)
 
-def get_fake_alerts() -> list:
+def random_fake_alerts() -> list:
     """
     Generates a list of dummy flood alerts for various locations in Brisbane. The number of alerts is randomly chosen
     between 1 and 3. Each alert is guaranteed to have a unique location. 
@@ -298,3 +298,21 @@ def get_fake_alerts() -> list:
         random_alerts.append(json.dumps(alert))
     
     return random_alerts
+
+
+
+def fake_alert(headline, location, risk, certainty, issue_date, expiry_date) -> str:
+    lat, lon = get_coordinates(f"Brisbane, {location}")
+    alert = {
+        "headline": headline,
+        "location": location,
+        "coordinates": (lat, lon),
+        "risk": risk,
+        "certainty": certainty,
+        "start": issue_date,
+        "end": expiry_date
+    }
+    alert["id"] = generate_unique_id(alert)
+    alert = json.dumps(alert)
+    return alert
+    
