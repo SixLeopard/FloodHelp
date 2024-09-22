251d24feeb8ef3ed11f33928237e727aed64759e
Jamie Westerhout <43029873+SixLeopard@users.noreply.github.com>
2024-09-22 14:04:25 +1000

    Update Relationships.py ot user relationship id instead of uid in approve

--- BackEnd/API/Relationships.py
+++ BackEnd/API/Relationships.py
@@ -106,7 +106,7 @@ def approve_relationship():
             relationship_id = request.form.get('relationship_id')
             uid = session["uid"]
             try:
-                relationships = db.get_relationships(uid)
+                relationships = db.get_relationships(relationship_id)
                 requestee_uid = relationships[relationship_id]["requestee_uid"]
                 if requestee_uid != session["uid"]:
                     return make_response({"current_user_not_requestee":1})
