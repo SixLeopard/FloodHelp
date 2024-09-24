fa2ad27f497c384ca181d338e69436d0224091c5
randy622 <randy.chakra@gmail.com>
2024-09-24 15:09:15 +1000

    Added methods that get and update alerts in the database

--- BackEnd/External_API/ExtApi_RealTime.py
+++ BackEnd/External_API/ExtApi_RealTime.py
@@ -151,7 +151,7 @@ def get_real_time_data() -> str:
 
 
 
-def get_alerts() -> list:
+def get_real_alerts() -> list:
 
     """
     Retrieves all flood alerts for Brisbane from the past day using an external weather API.
@@ -313,3 +313,8 @@ def fake_alert(headline, location, risk, certainty, issue_date, expiry_date) -> 
     alert = json.dumps(alert)
     return alert
     
+def are_alerts_equal(alert1, alert2):
+    if(alert1["headline"] == alert2["headline"] and alert1["location"] == alert2["location"] and alert1["coordinates"] == alert2["coordinates"] and alert1["risk"] == alert2["risk"] and alert1["certainty"] == alert2["certainty"] and alert1["start"] == alert2["start"] and alert1["end"] == alert2["end"]):
+        return True
+    else:
+        return False
