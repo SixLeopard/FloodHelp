b1734cd17c582149471dedea4fafbc923b5fde6f
randy622 <randy.chakra@gmail.com>
2024-09-27 11:35:45 +1000

    fixed another minor issue

--- BackEnd/External_API/ExtApi_RealTime.py
+++ BackEnd/External_API/ExtApi_RealTime.py
@@ -327,7 +327,7 @@ def specific_fake_alert(headline, location, risk, certainty, issue_date, expiry_
     return alert
     
 def are_alerts_equal(alert1, alert2):
-    if(alert1["headline"] == alert2[1] and alert1["location"] == alert2[2] and alert1["coordinates"] == alert2[7] and alert1["risk"] == alert2[3] and alert1["certainty"] == alert2[4] and alert1["start"] == alert2[5] and alert1["end"] == alert2[6]):
+    if(str(alert1["headline"]) == str(alert2[1]) and str(alert1["location"]) == str(alert2[2]) and str(alert1["coordinates"]) == str(alert2[7]) and str(alert1["risk"]) == str(alert2[3]) and str(alert1["certainty"]) == str(alert2[4]) and str(alert1["start"]) == str(alert2[5]) and str(alert1["end"]) == str(alert2[6])):
         return True
     else:
         return False
