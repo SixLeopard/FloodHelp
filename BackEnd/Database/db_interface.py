ee3e2bd610e8d249bc772efd79ff6e3f142a6b20
randy622 <randy.chakra@gmail.com>
2024-09-27 12:21:40 +1000

    test2

--- BackEnd/Database/db_interface.py
+++ BackEnd/Database/db_interface.py
@@ -735,7 +735,7 @@ class DBInterface():
         for alert in alerts:
             id = alert[0]
             expiry_date = alert[6]
-            print(compare_to_current_time(expiry_date) == "past")
+            print("time comparison:", compare_to_current_time(expiry_date) == "past")
             if(compare_to_current_time(expiry_date) == "past"):
                 try:
                     query = "DELETE FROM Alerts WHERE id = %s"
