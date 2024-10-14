d00a958840c53769f2ab72aa2afaede13fe10945
randy622 <randy.chakra@gmail.com>
2024-10-14 17:42:32 +1000

    Test 7

--- BackEnd/External_API/ExtApis_historical.py
+++ BackEnd/External_API/ExtApis_historical.py
@@ -68,8 +68,9 @@ def check_point(point: tuple):
             print(type(coords[0]))
             print(type(coords[0][0]))
             print(type(coords[0][0][0]))
+            print(coords[0][0][0])
             print(type(coords[0][0][0][0]))
-            print(type(coords[0][0][0][0][0]))
+            print(geo_type)
             if (is_point_in_polygon_or_multipolygon(geo_type, coords[0], point)):
                 return row
         except SyntaxError as e:
