76b1bcba1c2cde402c1dd81dff9a29d70a3c087e
Jamie Westerhout <43029873+SixLeopard@users.noreply.github.com>
2024-10-17 17:19:10 +1000

    Update CoordString_to_Tuple.py comments

--- BackEnd/Tools/CoordString_to_Tuple.py
+++ BackEnd/Tools/CoordString_to_Tuple.py
@@ -1,5 +1,17 @@
+###############################################
+# Description
+###############################################
+# Route for API that contains all the account
+# related routes and supporting functions
+###############################################
+# Setup
+###############################################
 
 def convert(coord : str) -> tuple[float,float]:
+    '''
+        simple function to convert cordiant formated strings
+        like (4324.324,32423.32) into a tuple of floats
+    '''
     parts = coord.replace('"', "").replace(')', "").replace('(', "").replace('[', "").replace(']', "").split(",")
     output = (float(parts[0].replace("{", "").replace("}", "")), float(parts[1].replace("{", "").replace("}", "")))
     return output
