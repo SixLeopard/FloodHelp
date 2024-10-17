457bdb7e8befa4dc3bc81667fcb6689084fc136c
Jamie Westerhout <43029873+SixLeopard@users.noreply.github.com>
2024-10-17 16:58:50 +1000

    Update database.py comments

--- BackEnd/API/database.py
+++ BackEnd/API/database.py
@@ -1,3 +1,19 @@
+###############################################
+# Description
+###############################################
+# contains global refrence to databse interface
+###############################################
+# File Info
+###############################################
+__author__ = 'FloodHelp BeckEnd Team'
+__copyright__ = 'Copyright 2024, FloodHelp API'
+__credits__ = ['Flask', 'Autodoc']
+__license__ = 'All Rights Reserved'
+__version__ = '0.8.9'
+__maintainer__ = 'FloodHelp BeckEnd Team'
+__status__ = 'Prototype'
+###############################################
+
 import Database.db_interface as db
 
 database_interface = db.DBInterface()
